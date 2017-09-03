d6bc249365759ad8ccfbc76e205f0c21d94ec64e
mbaxter <meredith.a.baxter@gmail.com>
2017-09-03 19:08:14 -0400

    Use async-await.

--- test-util/assert-transaction-fails.js
+++ test-util/assert-transaction-fails.js
@@ -2,10 +2,10 @@ const assert = require('chai').assert;
 
 const testRpcTxFailedRegex = /invalid opcode|invalid jump|out of gas/;
 const gethTxFailedRegex = /please check your gas amount/;
-const assertTransactionFails = function*(action, gasLimit, web3) {
+const assertTransactionFails = async (action, gasLimit, web3) => {
 	try {
-		let res = yield action();
-		const receipt = res.receipt || (yield web3.eth.getTransactionReceipt(res));
+		let res = await action();
+		const receipt = res.receipt || (await web3.eth.getTransactionReceipt(res));
 		assert.equal(receipt.gasUsed, gasLimit, "Failed transaction should consume all gas");
 	} catch (err) {
 		const errorMsg = (err + '').toLowerCase();
--- test/splitter-test.js
+++ test/splitter-test.js
@@ -1,4 +1,3 @@
-const co = require('co');
 const assertTxFailed = require('../test-util/assert-transaction-fails');
 let Splitter = artifacts.require("./Splitter.sol");
 const ethUtil = require('ethereumjs-util');
@@ -6,111 +5,93 @@ const ethUtil = require('ethereumjs-util');
 contract('Splitter', function(accounts) {
 
 	let gasLimit, splitter, txOptions;
-	before(() => {
-		return co(function*(){
-			gasLimit = 1000000;
-			splitter = yield Splitter.deployed();
-			txOptions = {gas: gasLimit, from:accounts[0]};
-		});
+	before(async () => {
+		gasLimit = 1000000;
+		splitter = await Splitter.deployed();
+		txOptions = {gas: gasLimit, from:accounts[0]};
 	});
 
 	describe('create contract', () => {
 
 		describe('with no recipients', () => {
-			it('should fail', () => {
-				return co(function*() {
-					const action = function*() {
-						return yield Splitter.new(0,0, {from: accounts[0]});
-					};
-					yield assertTxFailed(action, gasLimit, web3);
-				});
+			it('should fail', async () => {
+				const action = async () => {
+					return await Splitter.new(0,0, {from: accounts[0]});
+				};
+				await assertTxFailed(action, gasLimit, web3);
 			});
 		});
 
 		describe('with duplicate recipients', () => {
-			it('should fail', () => {
-				return co(function*() {
-					const action = function*() {
-						yield Splitter.new(accounts[1], accounts[1], {from: accounts[0]});
-					};
-					yield assertTxFailed(action, gasLimit, web3);
-				});
+			it('should fail', async () => {
+				const action = async () => {
+					await Splitter.new(accounts[1], accounts[1], {from: accounts[0]});
+				};
+				await assertTxFailed(action, gasLimit, web3);
 			});
 		});
 
 		describe('with first recipient equal to sender', () => {
-			it('should fail', () => {
-				return co(function*() {
-					const action = function*() {
-						return yield Splitter.new(accounts[0], accounts[2], txOptions);
-					};
-					yield assertTxFailed(action, gasLimit, web3);
-				});
+			it('should fail', async () => {
+				const action = async () => {
+					return await Splitter.new(accounts[0], accounts[2], txOptions);
+				};
+				await assertTxFailed(action, gasLimit, web3);
 			});
 		});
 
 		describe('with second recipient equal to sender', () => {
-			it('should fail', () => {
-				return co(function*() {
-					const action = function*() {
-						return yield Splitter.new(accounts[2], accounts[0], txOptions);
-					};
-					yield assertTxFailed(action, gasLimit, web3);
-				});
+			it('should fail', async () => {
+				const action = async () => {
+					return await Splitter.new(accounts[2], accounts[0], txOptions);
+				};
+				await assertTxFailed(action, gasLimit, web3);
 			});
 		});
 
 		describe('with first recipient equal to the contract', () => {
-			it('should fail', () => {
-				return co(function*() {
-					const action = function*() {
-						// From: https://github.com/b9lab/cyclical-reference/blob/master/migrations/2_deploy_contracts.js
-						const nonce = web3.eth.getTransactionCount(txOptions.from);
-						const contractAddress = ethUtil.bufferToHex(ethUtil.generateAddress(txOptions.from, nonce));
-						return yield Splitter.new(contractAddress, accounts[2], txOptions);
-					};
-					yield assertTxFailed(action, gasLimit, web3);
-				});
+			it('should fail', async () => {
+				const action = async () => {
+					// From: https://github.com/b9lab/cyclical-reference/blob/master/migrations/2_deploy_contracts.js
+					const nonce = web3.eth.getTransactionCount(txOptions.from);
+					const contractAddress = ethUtil.bufferToHex(ethUtil.generateAddress(txOptions.from, nonce));
+					return await Splitter.new(contractAddress, accounts[2], txOptions);
+				};
+				await assertTxFailed(action, gasLimit, web3);
 			});
 		});
 
 		describe('with second recipient equal to the contract', () => {
-			it('should fail', () => {
-				return co(function*() {
-					const action = function*() {
-						// From: https://github.com/b9lab/cyclical-reference/blob/master/migrations/2_deploy_contracts.js
-						const nonce = web3.eth.getTransactionCount(txOptions.from);
-						const contractAddress = ethUtil.bufferToHex(ethUtil.generateAddress(txOptions.from, nonce));
-						return yield Splitter.new(accounts[2], contractAddress, txOptions);
-					};
-					yield assertTxFailed(action, gasLimit, web3);
-				});
+			it('should fail', async () => {
+				const action = async () => {
+					// From: https://github.com/b9lab/cyclical-reference/blob/master/migrations/2_deploy_contracts.js
+					const nonce = web3.eth.getTransactionCount(txOptions.from);
+					const contractAddress = ethUtil.bufferToHex(ethUtil.generateAddress(txOptions.from, nonce));
+					return await Splitter.new(accounts[2], contractAddress, txOptions);
+				};
+				await assertTxFailed(action, gasLimit, web3);
 			});
 		});
 
 		describe('with distinct recipients not equal to sender or contract', () => {
-			it('should succeed', () => {
-				return co(function*(){
-					const splitter = yield Splitter.new(accounts[1], accounts[2], txOptions);
-					assert.ok(splitter);
-					assert.ok(splitter.address);
+			it('should succeed', async () =>{
+				const splitter = await Splitter.new(accounts[1], accounts[2], txOptions);
+				assert.ok(splitter);
+				assert.ok(splitter.address);
 
-					const recipients = yield splitter.recipients.call(txOptions);
-					assert.equal(recipients[0], accounts[1]);
-					assert.equal(recipients[1], accounts[2]);
-				});
+				const recipients = await splitter.recipients.call(txOptions);
+				assert.equal(recipients[0], accounts[1]);
+				assert.equal(recipients[1], accounts[2]);
 			});
 		});
 	});
 
 	describe('sendFunds()', () => {
 		let recipients;
-		beforeEach(() => {
-			return co(function*() {
-				splitter = yield Splitter.new(accounts[1], accounts[2], {from: accounts[0]});
-				assert(splitter, "New contract should be deployed");
-				recipients = [accounts[1], accounts[2]];
-			});
+		beforeEach(async () => {
+			splitter = await Splitter.new(accounts[1], accounts[2], {from: accounts[0]});
+			assert(splitter, "New contract should be deployed");
+			recipients = [accounts[1], accounts[2]];
 		});
 
 		describe('when passed a valid value', () => {
@@ -119,39 +100,29 @@ contract('Splitter', function(accounts) {
 
 				let txHash;
 				let value;
-				beforeEach(() => {
-					return co(function*() {
-						value = 10;
-						txHash = yield splitter.sendFunds({ value: value, gas: gasLimit, from:accounts[0] });
-					});
+				beforeEach(async () => {
+					value = 10;
+					txHash = await splitter.sendFunds({ value: value, gas: gasLimit, from:accounts[0] });
 				});
 
-				it('should succeed', () => {
-					return co(function*() {
-						assert.ok(txHash);
-						assert(txHash.receipt.gasUsed < gasLimit);
-					});
+				it('should succeed', async () => {
+					assert.ok(txHash);
+					assert(txHash.receipt.gasUsed < gasLimit);
 				});
 
-				it('should update recipientA\'s balance', () => {
-					return co(function*() {
-						const balance = yield splitter.balance.call(recipients[0]);
-						assert.equal(balance, value / 2);
-					});
+				it('should update recipientA\'s balance', async () => {
+					const balance = await splitter.balance.call(recipients[0]);
+					assert.equal(balance, value / 2);
 				});
 
-				it('should update recipientB\'s balance', () => {
-					return co(function*() {
-						const balance = yield splitter.balance.call(recipients[1]);
-						assert.equal(balance, value / 2);
-					});
+				it('should update recipientB\'s balance', async () => {
+					const balance = await splitter.balance.call(recipients[1]);
+					assert.equal(balance, value / 2);
 				});
 
-				it('should not update the owner\'s balance', () => {
-					return co(function*() {
-						const balance = yield splitter.balance.call(txOptions.from);
-						assert.equal(balance, 0);
-					});
+				it('should not update the owner\'s balance', async () => {
+					const balance = await splitter.balance.call(txOptions.from);
+					assert.equal(balance, 0);
 				});
 			});
 
@@ -159,39 +130,29 @@ contract('Splitter', function(accounts) {
 
 				let txHash;
 				let value;
-				beforeEach(() => {
-					return co(function*() {
-						value = 11;
-						txHash = yield splitter.sendFunds({ value: value, gas: gasLimit, from:accounts[0] });
-					});
+				beforeEach(async () => {
+					value = 11;
+					txHash = await splitter.sendFunds({ value: value, gas: gasLimit, from:accounts[0] });
 				});
 
-				it('should succeed', () => {
-					return co(function*() {
-						assert.ok(txHash);
-						assert(txHash.receipt.gasUsed < gasLimit);
-					});
+				it('should succeed', async () => {
+					assert.ok(txHash);
+					assert(txHash.receipt.gasUsed < gasLimit);
 				});
 
-				it('should update recipientA\'s balance', () => {
-					return co(function*() {
-						const balance = yield splitter.balance.call(recipients[0]);
-						assert.equal(balance, Math.floor(value / 2));
-					});
+				it('should update recipientA\'s balance', async () => {
+					const balance = await splitter.balance.call(recipients[0]);
+					assert.equal(balance, Math.floor(value / 2));
 				});
 
-				it('should update recipientB\'s balance', () => {
-					return co(function*() {
-						const balance = yield splitter.balance.call(recipients[1]);
-						assert.equal(balance, Math.floor(value / 2));
-					});
+				it('should update recipientB\'s balance', async () => {
+					const balance = await splitter.balance.call(recipients[1]);
+					assert.equal(balance, Math.floor(value / 2));
 				});
 
-				it('should not update the owner\'s balance', () => {
-					return co(function*() {
-						const balance = yield splitter.balance.call(txOptions.from);
-						assert.equal(balance, 1);
-					});
+				it('should not update the owner\'s balance', async () => {
+					const balance = await splitter.balance.call(txOptions.from);
+					assert.equal(balance, 1);
 				});
 			});
 		});
@@ -200,25 +161,21 @@ contract('Splitter', function(accounts) {
 
 			describe('of 1', () => {
 
-				it('should fail', () => {
-					return co(function*() {
-						const action = function*() {
-							return yield splitter.sendFunds({value: 1, gas: gasLimit, from:accounts[0]});
-						};
-						yield assertTxFailed(action, gasLimit, web3);
-					});
+				it('should fail', async () => {
+					const action = async () => {
+						return await splitter.sendFunds({value: 1, gas: gasLimit, from:accounts[0]});
+					};
+					await assertTxFailed(action, gasLimit, web3);
 				});
 			});
 
 			describe('of 0', () => {
 
-				it('should fail', () => {
-					return co(function*() {
-						const action = function*() {
-							return yield splitter.sendFunds({value: 0, gas: gasLimit, from:accounts[0]});
-						};
-						yield assertTxFailed(action, gasLimit, web3);
-					});
+				it('should fail', async () => {
+					const action = async () => {
+						return await splitter.sendFunds({value: 0, gas: gasLimit, from:accounts[0]});
+					};
+					await assertTxFailed(action, gasLimit, web3);
 				});
 			});
 		});
@@ -226,48 +183,38 @@ contract('Splitter', function(accounts) {
 
 	describe('withdrawFunds()', () => {
 
-		beforeEach(() => {
-			return co(function*() {
-				splitter = yield Splitter.new(accounts[1], accounts[2], {from: accounts[0]});
-				assert(splitter, "New contract should be deployed");
-			});
+		beforeEach(async () => {
+			splitter = await Splitter.new(accounts[1], accounts[2], {from: accounts[0]});
+			assert(splitter, "New contract should be deployed");
 		});
 
 		describe('for an account with no balance', () => {
-			it('should fail', () => {
-				return co(function*() {
-					const action = function*() {
-						return yield splitter.withdrawFunds({gas: gasLimit, from:accounts[3]});
-					};
-					yield assertTxFailed(action, gasLimit, web3);
-				});
+			it('should fail', async () => {
+				const action = async () => {
+					return await splitter.withdrawFunds({gas: gasLimit, from:accounts[3]});
+				};
+				await assertTxFailed(action, gasLimit, web3);
 			});
 		});
 
 		describe('for an account with a balance', () => {
 			let txHash;
 			let account;
-			beforeEach(() => {
-				return co(function*(){
-					const value = 10;
-					account = accounts[1];
-					yield splitter.sendFunds({ value, gas: gasLimit, from:accounts[0] });
-					txHash = yield splitter.withdrawFunds({ gas: gasLimit, from:account });
-				});
+			beforeEach(async () =>{
+				const value = 10;
+				account = accounts[1];
+				await splitter.sendFunds({ value, gas: gasLimit, from:accounts[0] });
+				txHash = await splitter.withdrawFunds({ gas: gasLimit, from:account });
 			});
 
-			it('should succeed', () => {
-				return co(function*() {
-					assert.ok(txHash);
-					assert(txHash.receipt.gasUsed < gasLimit);
-				});
+			it('should succeed', async () => {
+				assert.ok(txHash);
+				assert(txHash.receipt.gasUsed < gasLimit);
 			});
 
-			it('should zero-out the balance', () => {
-				return co(function*() {
-					const actualBalance = yield splitter.balance.call(account, txOptions);
-					assert.equal(actualBalance, 0);
-				});
+			it('should zero-out the balance', async () => {
+				const actualBalance = await splitter.balance.call(account, txOptions);
+				assert.equal(actualBalance, 0);
 			});
 		});
 	});
